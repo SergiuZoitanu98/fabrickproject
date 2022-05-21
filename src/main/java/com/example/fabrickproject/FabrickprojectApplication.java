6f02f1c3445b70f955032095842c31ed561ae347
SergiuZoitanu98 <szoitanu98@gmail.com>
2022-05-21 21:19:51 +0200

    stuff

---
+++ src/main/java/com/example/fabrickproject/FabrickprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabrickproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fabrickproject/FabrickprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabrickproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

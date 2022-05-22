c9f7683838f6abd2bae8dac3f28981664fb09641
SergiuZoitanu98 <szoitanu98@gmail.com>
2022-05-22 16:44:54 +0200

    added getbalance method

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,10 +1,12 @@
 package controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import utils.Utils;
+
+import java.io.IOException;
 
 @RestController
 public class MainController {
@@ -14,7 +16,7 @@ public class MainController {
 
 
     @GetMapping("/test")
-    public String getCose(@RequestBody String id){
+    public String getCose(@RequestParam String id) throws Exception {
     Utils utils = new Utils();
         return utils.getBalance(id);
     }
--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,7 +1,37 @@
 package utils;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Utils {
-    public String getBalance(String id){
-        return id;
+    private final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    public String getBalance(String id) throws Exception {
+        // open a connection, the request method is "GET" by default
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+"accountId="+id+"balance"))
+                .setHeader("Auth-Schema", "S2S")
+                .setHeader("Api-Key","FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP")
+
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print status code
+        System.out.println(response.statusCode());
+            String status = Integer.toString(response.statusCode());
+        // print response body
+        System.out.println(response.body());
+
+
+        return response.body();
     }
 }
